329c0862afdb1cf654132a7834878e4fb63242ba
ChickenHawkXP <47929127+ChickenHawkXP@users.noreply.github.com>
2020-03-07 19:59:31 -0500

    V1.2

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import webbrowser
 import sys
 from flask import *
 
+client = "b079374e3fdf4691a81109a7b2d94048"
+secret = "3a7d0a4ecb67452693c42835df2235dd"
+
 app = Flask(__name__)
 code = ''
 access_token = ""
@@ -17,7 +20,6 @@ params = urlencode({
     'redirect_uri': 'http://127.0.0.1:5000/',
     'response_type': 'code'
 })
-
 urlx = provider_url + '?' + params
 webbrowser.open(urlx)
 
@@ -89,6 +91,16 @@ def get_analysis(token,id):
     response = requests.get('https://api.spotify.com/v1/audio-features/%s'%id,headers=headers)
     return response.json()
 
+def get_recommendations(token):
+    headers = {
+    'Authorization': 'Bearer %s'%token,
+    }
+    genres = 'guitar'
+    dance = input('Enter your target dancibility: ')
+    instrumental = input('Enter your target instrumentalness: ')
+    response = requests.get('https://api.spotify.com/v1/recommendations?seed_genres=%s&target_danceability=%s&target_instrumentalness=%s'%(genres,dance,instrumental),headers=headers)
+    return response.json()
+
 def search_track(token):
     # Precondition: A unique string access code, passed by Spotify endpoint
     q = input("\nEnter a song name: ")
@@ -106,7 +118,34 @@ def search_track(token):
             print(j['name'],"-",i['name'],"-",dance['danceability'],"- %.2f"%((i['duration_ms']/1000)/60),'mins')
     return True
 
+def search_artist(token):
+    q = input("\nEnter an artists name: ")
+    if q == '-x-':
+        return False
+    q = q.replace(" ","%20")
+    headers = {
+    'Authorization': 'Bearer %s'%token,
+    }
+    response = requests.get('https://api.spotify.com/v1/search?q=%s&type=artist'%q,headers=headers)
+    for i in response.json()['artists']['items']:
+        print(i['name'],"-",i['followers']['total'],"followers -",i['genres'])
+    return response.json()
+
+def get_artist(token):
+    q = input("\nEnter an artists name: ")
+    if q == '-x-':
+        return False
+    q = q.replace(" ","%20")
+    headers = {
+    'Authorization': 'Bearer %s'%token,
+    }
+    response = requests.get('https://api.spotify.com/v1/search?q=%s&type=artist'%q,headers=headers)
+    return response.json()
+
 def main():
+    headers = {
+    'Authorization': 'Bearer %s'%access_token,
+    }
     artists = get_user_artists(access_token)
     tracks = get_user_tracks(access_token)
 
@@ -125,23 +164,49 @@ def main():
             spotifytracks.write(str(new_name).strip('b').strip('\'').strip('"')+'\t%f\n'%dance['danceability'])
     
     choice = 0
-    flag = True
-    while(choice != '3'):
-        print("\n[1] Search by Track\n[2] Get playlist\n[3] Quit")
+    while(choice != '5'):
+        print("\n[1] Search by Track\n[2] Search by Artist\n[3] Get Playlist\n[4] Get Recommendations\n[5] Quit")
         choice = input("What would you like to do?: ")
+        flag = True
         if choice == '1':
             while flag:
                 print("Enter '-x-' to exit")
                 flag = search_track(access_token)
         if choice == '2':
+            while flag:
+                print("Enter '-x-' to exit")
+                flag = search_artist(access_token)
+        if choice == '3':
             avg_dance = 0.0
             avg_count = 0
             playlist_id = input("Input the playlist uri: ")
             playlist_tracks = get_playlist(access_token,playlist_id[17::])
+            next_call = playlist_tracks['tracks']['next']
             for i in playlist_tracks['tracks']['items']:
                 ids = i['track']['id']
                 dance = get_analysis(access_token,ids)
                 avg_dance += float(dance['danceability'])
                 avg_count += 1
-                print(i['track']['name'],"-",dance['danceability'])      
+                print(i['track']['name'],"-",dance['danceability']) 
+            
+            total_songs = playlist_tracks['tracks']['total']
+            if next_call != None: #Gets next page of playlist, limit at 200
+                next_page = requests.get(next_call,headers=headers)
+                for i in next_page.json()['items']:
+                    ids = i['track']['id']
+                    dance = get_analysis(access_token,ids)
+                    avg_dance += float(dance['danceability'])
+                    avg_count += 1
+                    print(i['track']['name'],"-",dance['danceability'])
+            
+            print('Total: %s songs'%total_songs)
             print("Danceability average of this playlist: %.2f"%(avg_dance/avg_count))
+        if choice == '4':
+            recommendations = get_recommendations(access_token)
+            for i in recommendations['tracks']:
+                ids = i['id']
+                for j in i['artists']:
+                    artist = j['name']
+                    break
+                dance = get_analysis(access_token,ids)
+                print(artist,"-",i['name'],'-',dance['danceability'])
