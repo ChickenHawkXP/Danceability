1e052788738a9d44073594f2dd812b1d6f330541
ChickenHawkXP <47929127+ChickenHawkXP@users.noreply.github.com>
2020-03-30 00:17:32 -0400

    Reducing Code

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import json
 import requests
 import base64
+import pandas as pd
 from urllib.parse import urlencode
 import webbrowser
 import sys
+import spotifyAPI as API
 from flask import *
+from parseJSON import parse_json as parser
 
-client = "b079374e3fdf4691a81109a7b2d94048"
-secret = "3a7d0a4ecb67452693c42835df2235dd"
+client = ""
+secret = ""
 
+spotify = API.spotify_api(client,secret)
 app = Flask(__name__)
 code = ''
 access_token = ""
 provider_url = "https://accounts.spotify.com/authorize"
 
-params = urlencode({
-    'client_id': '%s'%client,
-    'scope': 'user-top-read',
-    'redirect_uri': 'http://127.0.0.1:5000/',
-    'response_type': 'code'
-})
-urlx = provider_url + '?' + params
+urlx = spotify.get_url()
 webbrowser.open(urlx)
 
 @app.route("/")
@@ -31,64 +29,15 @@ def spotify_callback():
     rurl = str(request.url) # Some jank way to get the code ????
     scode = rurl.find("=")
     code = rurl[scode+1::]
-    access_token = get_access_token()
+    access = spotify.get_access_token(code)
     main()
     return "Thank you very much!"
-
-def get_access_token():
-    # Use code to retrieve access code from Spotify API
-    codes = client+":"+secret
-    encode = codes.encode('ascii')
-    head = base64.b64encode(encode) # Client and secret codes need to be base64 encoded and passed to API
-
-    headers = {
-    'Authorization': 'Basic %s'%head.decode('ascii'),
-    }   
-    data = {
-    'grant_type': 'authorization_code',
-    'code': '%s'%code,
-    'redirect_uri': 'http://127.0.0.1:5000/'
-    }
-    
-    response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
-    try:
-        response.json()['access_token']
-    except:
-        print(response.json())
-        quit()
-    return response.json()['access_token'] #Return access code
-
-def get_user_artists(token):
-    # Obtain user's (who confirmed website) top artists
-    headers = {
-    'Authorization': 'Bearer %s'%token,
-    }
-    response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers)
-    return response.json()
-
-def get_user_tracks(token):
-    # Precondition: A unique string access code, passed by Spotify endpoint
-    # Obtain user's (who confirmed website) top tracks
-    headers = {
-    'Authorization': 'Bearer %s'%token,
-    }
-    response = requests.get('https://api.spotify.com/v1/me/top/tracks', headers=headers)
-    return response.json()
 
 def get_playlist(token,playlistid):
     headers = {
     'Authorization': 'Bearer %s'%token,
     }
     response = requests.get('https://api.spotify.com/v1/playlists/%s'%playlistid, headers=headers)
-    return response.json()
-
-def get_analysis(token,id):
-    # Precondition: A unique string access code, passed by Spotify endpoint and a unqiue string belonging to each spotify track
-    # Obtain Spotify's track analysis (Danceability mainly)
-    headers = {
-    'Authorization': 'Bearer %s'%token,
-    }
-    response = requests.get('https://api.spotify.com/v1/audio-features/%s'%id,headers=headers)
     return response.json()
 
 def get_recommendations(token):
@@ -103,6 +52,10 @@ def get_recommendations(token):
 
 def search_track(token):
     # Precondition: A unique string access code, passed by Spotify endpoint
+    artist_name = []
+    song_name = []
+    dancibility = []
+    song_duration = []
     q = input("\nEnter a song name: ")
     if q == '-x-':
         return False
@@ -115,7 +68,17 @@ def search_track(token):
         id = i['id']
         for j in i['album']['artists']:
             dance = get_analysis(access_token,id)
-            print(j['name'],"-",i['name'],"-",dance['danceability'],"- %.2f"%((i['duration_ms']/1000)/60),'mins')
+            artist_name.append(j['name'])
+            song_name.append(i['name'])
+            dancibility.append(dance['danceability'])
+            mins = i['duration_ms']/1000/60
+            song_duration.append(mins)
+            #print(j['name'],"-",i['name'],"-",dance['danceability'],"- %.2f"%((i['duration_ms']/1000)/60),'mins')
+    tracks = pd.DataFrame({'Artist':artist_name,
+                           'Song Name':song_name,
+                           'Dancibility':dancibility,
+                           'Duration':song_duration})
+    print(tracks)
     return True
 
 def search_artist(token):
@@ -146,25 +109,12 @@ def main():
     headers = {
     'Authorization': 'Bearer %s'%access_token,
     }
-    artists = get_user_artists(access_token)
-    tracks = get_user_tracks(access_token)
-
-    with open ("spotifylist.txt","w") as spotifyartists:
-        spotifyartists.write("Top Artists:\tPopularity:\n")
-        for artist in artists['items']:
-            spotifyartists.write(artist['name']+'\t\t'+str(artist["popularity"])+"\n")
-
-    with open ('spotifytracks.txt','w') as spotifytracks:
-        spotifytracks.write("Top Songs:\tDanceability\n")
-        for track in tracks['items']:
-            ids = track['id']  
-            dance = get_analysis(access_token,ids)
-            name = track['name'].encode(sys.stdout.encoding, errors='replace') # Encode random symbols in certain song title
-            new_name = str(name).replace('\\xe2\\x97\\x90','').replace('\\xe2\\x97\\x91','') # Replace the unknown characters with nothing
-            spotifytracks.write(str(new_name).strip('b').strip('\'').strip('"')+'\t%f\n'%dance['danceability'])
     
+    write_to_txt()
+
     choice = 0
     while(choice != '5'):
+
         print("\n[1] Search by Track\n[2] Search by Artist\n[3] Get Playlist\n[4] Get Recommendations\n[5] Quit")
         choice = input("What would you like to do?: ")
         flag = True
@@ -210,3 +160,33 @@ def main():
                     break
                 dance = get_analysis(access_token,ids)
                 print(artist,"-",i['name'],'-',dance['danceability'])
+
+def write_to_txt(): 
+
+    track_name = []
+
+    artists = spotify.get_user_artists()
+    tracks = spotify.get_user_tracks()
+
+    with open ("spotifylist.txt","w") as spotifyartists:
+        artist_name = parser.extract_values(artists,'name')
+        artist_popularity = parser.extract_values(artists,'popularity')
+
+        top_artists = pd.DataFrame({"Top Artists:":artist_name,         # Use panda's to organize data better
+                                    "Popularity":artist_popularity})  
+        spotifyartists.write(top_artists.to_string())
+
+    with open ('spotifytracks.txt','w') as spotifytracks:
+        for track in tracks['items']:
+            name = track['name'].encode(sys.stdout.encoding, errors='replace') # Encode random symbols in certain song title
+            new_name = str(name).replace('\\xe2\\x97\\x90','').replace('\\xe2\\x97\\x91','') # Replace the unknown characters with nothing
+            track_name.append(str(new_name).strip('b').strip('\'').strip('"'))
+
+        track_ids = parser.extract_values(tracks,'id')
+        dance = spotify.get_analysis(track_ids)
+        danceability = parser.extract_values(dance,'danceability')
+
+        track_list = pd.DataFrame({"Track Name":track_name,
+                                       "Danceability:":danceability})
+
+        spotifytracks.write(track_list.to_string())
